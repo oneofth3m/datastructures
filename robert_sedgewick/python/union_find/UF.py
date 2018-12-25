87b920490fb89ce8879b2ed50b15cd18b2fc4c2f
Naveen G <nawin.g@gmail.com>
2018-12-25 09:38:52 +0530

    UNION-FIND v1

---
+++ robert_sedgewick/python/union_find/UF.py
@@ -0,0 +1,22 @@
+class UF:
+    def __init__(self, size):
+        self._a = [i for i in xrange(size)]
+
+    def find(self, p, q):
+        return self._a[p] == self._a[q]
+
+    def union(self, p, q):
+        idx_p = self._a[p]
+        idx_q = self._a[q]
+
+        self._a = map(lambda x: idx_p if x == idx_q else x, self._a)
+
+def main():
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
